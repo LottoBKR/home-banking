e8702474fe78066d226eaa236df2bb6b5ad3d355
LottoBKR <leonardobueras247@gmail.com>
2024-10-07 09:09:58 -0300

    Commint inicial del proyecto

---
+++ src/main/java/com/mindhub/homebanking/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.mindhub.homebanking;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HomebankingApplication.class);
+	}
+
+}
